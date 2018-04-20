20d87bccd4e009669291fc7aff36f2bcd5924bb3
Jared <jaredwads@gmail.com>
2018-04-19 20:36:59 -0600

    Added Executor

--- executor.py
+++ executor.py
@@ -1,2 +1,47 @@
 class Executor:
-    pass
+    def __init__(self, initial_balance=100000):
+        self.buy_queue = []
+        self.trades_in_market = []
+        self.account_balance = initial_balance
+
+    def in_market(self):
+        return len(self.trades_in_market) > 0
+
+    def consume(self, event):
+        if event.type == 'MARKET':
+            self.buy_queue.append(event)
+
+        if event.type == 'TICK':
+            while len(self.buy_queue) > 0:
+                self.execute_trade(self.buy_queue.pop(), event.candle.open)
+
+    def execute_trade(self, market_order, price):
+        if len(self.trades_in_market) == 0:
+            self.trades_in_market.append(Order(market_order, price))
+
+        else:
+            for order in list(self.trades_in_market):
+                if order.direction == market_order.direction:
+                    order.quantity += market_order.quantity
+                elif order.quantity > market_order.quantity:
+                    order.quantity -= market_order.quantity
+                else:
+                    self.adjust_balance(order, price, -1)
+                    market_order.quantity -= order.quantity
+                    self.trades_in_market.remove(order)
+
+                    if market_order.quantity > 0:
+                        self.trades_in_market.append(market_order)
+
+        self.adjust_balance(market_order, price)
+
+    def adjust_balance(self, order, price, enter=1):
+        self.account_balance -= order.quantity * price * enter * (1 if order.direction == 'BUY' else -1)
+
+
+class Order:
+    def __init__(self, market_event, price):
+        self.direction = market_event.direction
+        self.quantity = market_event.quantity
+        self.instrument = market_event.instrument
+        self.price = price
---
+++ test/executor_test.py
@@ -0,0 +1,41 @@
+import unittest
+
+from Candle import Candle
+from Events.Market import Market
+from Events.Tick import Tick
+from Executor import Executor
+
+
+class ExecutorTest(unittest.TestCase):
+    def setUp(self):
+        self.executor = Executor()
+
+    def test_should_enter(self):
+        self.assertFalse(self.executor.in_market())
+        self.executor.consume(Market("BUY", 1000))
+        self.executor.consume(Tick(Candle(1, 1, 1, 1), 'H'))
+        self.assertTrue(self.executor.in_market())
+
+    def test_should_exit(self):
+        self.executor.consume(Market("BUY", 1000))
+        self.executor.consume(Tick(Candle(1, 1, 1, 1), 'H'))
+        self.assertTrue(self.executor.in_market())
+        self.executor.consume(Market("SELL", 1000))
+        self.executor.consume(Tick(Candle(1, 1, 1, 1), 'H'))
+        self.assertFalse(self.executor.in_market())
+
+        # Now test the opposite
+        self.executor.consume(Market("SELL", 1000))
+        self.executor.consume(Tick(Candle(1, 1, 1, 1), 'H'))
+        self.assertTrue(self.executor.in_market())
+        self.executor.consume(Market("BUY", 1000))
+        self.executor.consume(Tick(Candle(1, 1, 1, 1), 'H'))
+        self.assertFalse(self.executor.in_market())
+
+    def test_should_calculate_earnings(self):
+        self.executor.account_balance = 1000
+        self.executor.consume(Market("BUY", 1000))
+        self.executor.consume(Tick(Candle(1, 1, 1, 1), 'H'))
+        self.executor.consume(Market("SELL", 1000))
+        self.executor.consume(Tick(Candle(2, 2, 2, 2), 'H'))
+        self.assertEqual(self.executor.account_balance, 2000)
